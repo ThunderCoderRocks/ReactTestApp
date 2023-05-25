cec3b7e7125630a9a764802dde682e59339e33e1
abhijeetsinha9 <40666114+abhijeetsinha9@users.noreply.github.com>
2023-05-25 13:05:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         My First Netfliy Deployment
+         Welcome to Big Tall Tree
         </p>
         <a
           className="App-link"
